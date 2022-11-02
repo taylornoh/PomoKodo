9e9ea92e776d706b4d5fa6ad1ae665c81846b106
Taylor Noh <taylornoh99@gmail.com>
2022-11-01 20:39:15 -0700

    Added documentation for timer.

--- timer.js
+++ timer.js
@@ -1,7 +1,11 @@
+// Constants values defined for this file
 const FULL_DASH_ARRAY = 283;
+// Warning occurs at 10s
 const WARNING_THRESHOLD = 10;
+// Alert occurs at 5s
 const ALERT_THRESHOLD = 5;
 
+// Defining variables with specific properties like color and threshold
 const COLOR_CODES = {
   info: {
     color: "green"
@@ -16,12 +20,20 @@ const COLOR_CODES = {
   }
 };
 
+// Defining variables that are limited ot the scope of a block statement, or expresion on which it is used
 const TIME_LIMIT = 1200;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
 let remainingPathColor = COLOR_CODES.info.color;
 
+// Grabbing timer API
+// document.getElementById returns an element with a specified value
+// Creating a basic template for timer
+// Adding a Scalable vector graphic with a circl element inside to draw a timer ring that will
+// indicate the passing time and add a spant to show the remaining time value
+// We are writing the html in JavaScript and injecting into the DOM by targeting the #app element
+// DOM = Document Object Model
 document.getElementById("Countdown").innerHTML = `
 <div class="base-timer">
   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
@@ -46,14 +58,21 @@ document.getElementById("Countdown").innerHTML = `
 </div>
 `;
 
+// Calling startTimer function
 startTimer();
 
+// Defining a function that clears the timer
 function onTimesUp() {
+  // clearInterval is a built-in function
   clearInterval(timerInterval);
 }
 
+// Defining a function that starts the timer
 function startTimer() {
+  // setInterval is a built-in function that is used to set a delay
+  // You can cancel the interval using clearInterval()
   timerInterval = setInterval(() => {
+    // Timer logic
     timePassed = timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
     document.getElementById("base-timer-label").innerHTML = formatTime(
@@ -69,18 +88,25 @@ function startTimer() {
 }
 
 function formatTime(time) {
+  // The largest round integer less than or equal to the result of time divided being by 60.
   const minutes = Math.floor(time / 60);
+
+  // Seconds are the remainder of the time divided by 60 (modulus operator)
   let seconds = time % 60;
 
+  // If the value of seconds is less than 10, then display seconds with a leading zero
   if (seconds < 10) {
     seconds = `0${seconds}`;
   }
 
+  // The output in MM:SS format
   return `${minutes}:${seconds}`;
 }
 
+// Creating a function that defines the timer color based on how much time left
 function setRemainingPathColor(timeLeft) {
   const { alert, warning, info } = COLOR_CODES;
+  // If the remaining time is less than or equal to 5, remove the "warning" class and apply the "alert" class.
   if (timeLeft <= alert.threshold) {
     document
       .getElementById("base-timer-path-remaining")
@@ -88,6 +114,7 @@ function setRemainingPathColor(timeLeft) {
     document
       .getElementById("base-timer-path-remaining")
       .classList.add(alert.color);
+  // If the remaining time is less than or equal to 10, remove the base color and apply the "warning" class.
   } else if (timeLeft <= warning.threshold) {
     document
       .getElementById("base-timer-path-remaining")
@@ -98,11 +125,13 @@ function setRemainingPathColor(timeLeft) {
   }
 }
 
+// Divides time left by the defined time limit
 function calculateTimeFraction() {
   const rawTimeFraction = timeLeft / TIME_LIMIT;
   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
 
+// Updating the dasharray value as time passes, starting with 283
 function setCircleDasharray() {
   const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
